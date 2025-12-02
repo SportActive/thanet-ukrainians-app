6fe09b5777c9e52440af2d831689d33c8f65a8b4
Kyrylo <kirill.grig.2000@gmail.com>
2025-12-02 02:08:16 +0200

    First commit: Full working app with Admin Dashboard

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.get('/', (req, res) => {
   res.send('Server is running with CORS: *');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server працює на порті ${PORT}`);
